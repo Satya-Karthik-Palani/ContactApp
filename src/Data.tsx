e1d1a7e11060ba8f24ac1ae823b63aef3bf8d970
Satya-Karthik-Palani <karthik.palani0827@gmail.com>
2024-04-30 14:19:46 +0530

    first commit

---
+++ src/Data.tsx
@@ -0,0 +1,14 @@
+export const contactList = [
+    {
+        firstname : "karthik",
+        lastname : "palani",
+        status: true,
+        id: 1
+    },
+    {
+        firstname : "subhash",
+        lastname : "kristipati",
+        status: true,
+        id: 1
+    },
+]
---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {Provider} from "react-redux";
+import {configureStore} from "@reduxjs/toolkit";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const store = configureStore({
+  reducer:{
+    
+  }
+})
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
